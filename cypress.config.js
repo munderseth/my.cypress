3321e5cd3138c8c6180dd2d4bee9a2b6c6c1a97e
munderseth <marku@s2technologies.com>
2023-02-25 08:42:44 -0800

    add cypress workflow

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "g4ggq1",
   reporter: 'junit',
   reporterOptions: {
     mochaFile: 'cypress/results/results.[hash].xml',
